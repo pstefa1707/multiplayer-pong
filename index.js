03ef44adca964246a99b519f928a3becd1d0b3bc
Paras <stefap@smc.sa.edu.au>
2019-09-30 13:34:43 +0930

    improved mobile support

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ var express = require('express');
 var Game = require('./Game');
 var app = express();
 const HERTZ = 30; //Game updates per second
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 80;
 var server = require('http')
 	.createServer(app)
-	.listen(port, '192.168.0.29');
+	.listen(port);
 var io = require('socket.io')(server);
 
 app.use(express.static(__dirname + '/node_modules'));
